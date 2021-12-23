c3cbbe26d69a7621aec1a76cd6e447cd018086dc
Elisa Mesquita <elisamesquita58@gmail.com>
2021-12-23 14:46:03 -0300

    Adicionei fulcalendar, linkei com o banco

--- api/index.js
+++ api/index.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { json } = require('express/lib/response');
+const { json, append } = require('express/lib/response');
 const res = require('express/lib/response');
+const reservas = require('./controller/ReservaController.js');
+const cors = require('cors')
+
 
 const app = express();
-
+app.use(cors())
 app.use(bodyParser.json());
 
 const port = 3000;
 
-app.get('/agendar', (req, res) => res
-    .status(200)
-    .send({mensagem:'Conectado!'})
-);
+app.get('/agendar', async (req, res) => {
+    await reservas.pegAllReservas(req, res)
+});
 
 app.listen(port, () => console.log(`Conectado na porta ${port} :)`));
 
--- api/seeders/20211223023513-sreserva.js
+++ api/seeders/20211223023513-sreserva.js
@@ -13,12 +13,20 @@ module.exports = {
       },
       {
         title: "Autocode",
-        start: "2021-12-27 12:30:00",
+        start: "2021-12-27 15:30:00",
         end: "2021-12-27 18:00:00",
         color: "#2596be",
         createdAt: new Date(),
         updatedAt: new Date()
       }
+      // {
+      //   title: "Autocode",
+      //   start: "2021-12-27 12:30:00",
+      //   end: "2021-12-27 18:00:00",
+      //   color: "#2596be",
+      //   createdAt: new Date(),
+      //   updatedAt: new Date()
+      // }
     ], {});
   },
 
